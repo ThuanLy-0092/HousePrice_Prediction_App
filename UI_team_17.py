6b4a62b89e5aae25e374840c730764ae9dd99b62
ThuanLy-0092 <133451381+ThuanLy-0092@users.noreply.github.com>
2024-06-20 22:01:16 +0700

    Update UI_team_17.py

--- UI_team_17.py
+++ UI_team_17.py
@@ -38,7 +38,7 @@ def haversine(lon1, lat1, lon2, lat2):
 
     # Công thức Haversine
     dlon = lon2 - lon1
-    dlat = lon2 - lat2
+    dlat = lat2 - lat1
     a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
     c = 2 * np.arcsin(np.sqrt(a))
 
